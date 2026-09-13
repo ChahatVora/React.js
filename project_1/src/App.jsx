d67650f466f38f2ea0fa4effb185c0bc35974b30
Chahat Vora <chahatvora1008@gmail.com>
2026-09-13 23:13:52 +0530

    Counter project

--- project_1/src/App.jsx
+++ project_1/src/App.jsx
@@ -1,11 +1,29 @@
 import "./App.css";
-import Vr from "./Chahat.jsx";
+import { useState } from "react";
 
 function App() {
+  const [counter, setCounter] = useState(10)
+
+  const addValue = () => {
+    if (counter >= 0 & counter <= 19){
+      setCounter(counter + 1)
+    }
+    
+  }
+  const removeValue = () => {
+    if (counter >= 1 & counter <= 20){
+      setCounter(counter - 1)
+    }
+  }
   return (
     <>
-      <h1>vr chahat</h1>
-      <Vr/>
+      <h1>Counter project</h1>
+      <p>Counter:- {counter}</p>
+
+      <button onClick={addValue}>add value:- {counter}</button>
+      <button onClick={removeValue}>remove value:- {counter}</button>
+      <p>final value:- {counter}</p>
+      
     </>
   );
 }
